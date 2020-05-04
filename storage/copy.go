96f4b84a5a5c42980173bb08f9dd631cf087ca29
yoneyan <github@yoneyan.net>
2020-05-04 19:57:52 +0900

    add copy image file

--- storage/copy.go
+++ storage/copy.go
@@ -4,26 +4,24 @@ import (
 	"io"
 	"log"
 	"os"
+	"strconv"
+	"time"
 )
 
-type fileTransfer struct {
+type FileTransfer struct {
 	io.Reader
 	total    int64
 	fileSize int64
 }
 
-func (ft *fileTransfer) Read(p []byte) (int, error) {
+func (ft *FileTransfer) Read(p []byte) (int, error) {
 	n, err := ft.Reader.Read(p)
 	ft.total += int64(n)
-
-	if err == nil {
-		log.Println("Progress: ", ft.total, " FileSize: ", ft.fileSize)
-	}
 	return n, err
 }
 
-func fileCopy(srcFile, dstFile string) result {
-	log.Println("---Copy Image")
+func (t *Tmp) fileCopy(srcFile, dstFile string) result {
+	log.Println("---Copy disk image")
 	log.Println("src: " + srcFile)
 	log.Println("dst: " + dstFile)
 	src, err := os.Open(srcFile)
@@ -45,11 +43,20 @@ func fileCopy(srcFile, dstFile string) result {
 	}
 	defer dst.Close()
 
-	tmp := fileTransfer{Reader: src, fileSize: file.Size()}
+	tmp := FileTransfer{Reader: src, fileSize: file.Size()}
+	copy := true
+	go func() {
+		for tmp.fileSize != tmp.total && copy == true {
+			timer := time.NewTimer(time.Second * 1)
+			<-timer.C
+			t.Info = "Progress: " + strconv.Itoa(int(tmp.total)) + " FileSize: " + strconv.Itoa(int(tmp.fileSize))
+		}
+	}()
 
 	_, err = io.Copy(dst, &tmp)
 	if err != nil {
 		log.Println("Error: file copy error")
+		copy = false
 		return result{Info: "Error: file copy error", Err: err}
 	}
 	return result{Info: "OK", Err: nil}
