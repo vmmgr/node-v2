83f1c813a3847ebf3d765a011663115d33965e81
yoneyan <github@yoneyan.net>
2020-05-28 17:02:34 +0900

    wip vm process

--- data/vm.go
+++ data/vm.go
@@ -3,224 +3,127 @@ package data
 import (
 	"context"
 	"fmt"
-	"github.com/yoneyan/vm_mgr/node/db"
-	"github.com/yoneyan/vm_mgr/node/manage"
-	"github.com/yoneyan/vm_mgr/node/vm"
-	pb "github.com/yoneyan/vm_mgr/proto/proto-go"
+	"github.com/vmmgr/node/db"
+	pb "github.com/vmmgr/node/proto/proto-go"
+	"github.com/vmmgr/node/vm"
 	"log"
+	"strconv"
+	"strings"
 )
 
-func (s *server) CreateVM(ctx context.Context, in *pb.VMData) (*pb.Result, error) {
-	fmt.Println("----------CreateVM-----")
-	log.Printf("Receive VMID        : %v", in.GetOption().GetId())
-	log.Printf("Receive Type        : %v", in.GetType())
-	log.Printf("Receive name        : %v", in.GetVmname())
-	log.Printf("Receive cpu         : %v", in.GetVcpu())
-	log.Printf("Receive mem         : %v", in.GetVmem())
-	log.Printf("Receive StoragePath : %v", in.GetOption().StoragePath)
-	log.Printf("Receive Storage     : %v", in.GetStorage())
-	log.Printf("Receive CDROM       : %v", in.GetCdrom())
-	log.Printf("Receive vnc         : %v", in.GetOption().Vnc)
-	log.Printf("Receive net         : %v", in.GetVnet())
-	log.Printf("Receive autostart   : %v", in.GetOption().Autostart)
-	var r vm.CreateVMInformation
+func (s *server) AddVM(ctx context.Context, in *pb.VMData) (*pb.Result, error) {
+	log.Println("----------AddVM-----")
+	log.Printf("Receive Name     : %v", in.GetName())
+	log.Printf("Receive GroupID  : %v", in.GetGroupID())
+	log.Printf("Receive CPU      : %v", in.GetCPU())
+	log.Printf("Receive Mem      : %v", in.GetMem())
+	log.Printf("Receive Boot     : %v", in.GetBoot())
+	log.Printf("Receive ISO      : %v", in.GetISO())
+	log.Printf("Receive Storage  : %v", in.GetStorage())
+	log.Printf("Receive Net      : %v", in.GetNet())
+	log.Printf("Receive PCI      : %v", in.GetPCIData())
+	log.Printf("Receive Auto     : %v", in.GetAutoStart())
 
-	r.ID = int(in.GetOption().Id)
-	r.Name = in.GetVmname()
-	r.CPU = int(in.GetVcpu())
-	r.Mem = int(in.GetVmem())
-	r.Storage = in.GetStorage()
-	r.CDROM = in.GetOption().CdromPath
-	r.Net = in.GetVnet()
-	r.VNC = int(in.GetOption().Vnc)
-	r.AutoStart = in.GetOption().Autostart
-
-	if in.GetType()/10 == 1 {
-		//Disk copy process
-		log.Printf("AutoVMProcess")
-		go vm.CreateAutoVMProcess(in)
-		return &pb.Result{Status: true, Info: "Add process.. wait...."}, nil
+	if result := vm.AddVM(in); result.Err != nil {
+		return &pb.Result{Status: false, Info: result.Info + "ErrorLog: " + fmt.Sprint(result.Err)}, nil
 	}
-
-	r.StoragePath = manage.StorageProcess(in)
-
-	info, result := vm.CreateVMProcess(&r)
-	if result {
-		return &pb.Result{Status: true, Info: "ok!!"}, nil
-	}
-	return &pb.Result{Status: false, Info: info}, nil
+	return &pb.Result{Status: true, Info: "ok"}, nil
 }
 
-func (s *server) DeleteVM(ctx context.Context, in *pb.VMID) (*pb.Result, error) {
+func (s *server) DeleteVM(ctx context.Context, in *pb.VMData) (*pb.Result, error) {
 	fmt.Println("----------DeleteVM-----")
-	log.Printf("Receive VMID: %v", in.GetId())
+	log.Printf("Receive ID: %v", in.GetID())
 
-	info, result := vm.DeleteVMProcess(int(in.GetId()))
-	if result {
-		fmt.Println("Delete success!!")
-		return &pb.Result{Status: true, Info: "ok"}, nil
+	if result := vm.DeleteVM(in); result.Err != nil {
+		return &pb.Result{Status: false, Info: result.Info + "ErrorLog: " + fmt.Sprint(result.Err)}, nil
+	}
+	return &pb.Result{Status: true, Info: "ok"}, nil
+}
+
+func (s *server) UpdateVM(ctx context.Context, in *pb.VMData) (*pb.Result, error) {
+	fmt.Println("----------UpdateVM-----")
+	log.Printf("Receive ID: %v", in.GetID())
+
+	if result := vm.UpdateVM(in); result.Err != nil {
+		return &pb.Result{Status: false, Info: fmt.Sprint(result.Err)}, nil
 	} else {
-		fmt.Println("Delete Failed....")
-		return &pb.Result{Status: false, Info: info}, nil
+		return &pb.Result{Status: true, Info: "OK"}, nil
 	}
 }
 
-func (s *server) StartVM(ctx context.Context, in *pb.VMID) (*pb.Result, error) {
-	fmt.Println("----------StartVM-----")
-	log.Printf("Receive VMID: %v", in.GetId())
-	info, result := vm.StartVMProcess(int(in.GetId()))
-	if result {
-		return &pb.Result{Status: true, Info: "ok"}, nil
+func (s *server) GetVM(ctx context.Context, in *pb.VMData) (*pb.VMData, error) {
+	fmt.Println("----------GetVM-----")
+	log.Printf("Receive ID: %v", in.GetID())
+	var storage []*pb.StorageData
+	var net []*pb.NetData
+	var pci []*pb.PCIData
+
+	if data, err := db.SearchDBVM(db.VM{ID: int(in.GetID())}); err != nil {
+		return &pb.VMData{}, err
 	} else {
-		return &pb.Result{Status: false, Info: info}, nil
+		for _, tmp := range strings.Split(data.Storage, ",") {
+			value, _ := strconv.Atoi(tmp)
+			storage = append(storage, &pb.StorageData{ID: int64(value)})
+		}
+		for _, tmp := range strings.Split(data.Net, ",") {
+			value, _ := strconv.Atoi(tmp)
+			net = append(net, &pb.NetData{ID: int64(value)})
+		}
+		//for _, tmp := range strings.Split(data.PCI, ",") {
+		//	value, _ := strconv.Atoi(tmp)
+		//	pci = append(pci, &pb.PCIData{ID: int64(value)})
+		//}
+
+		return &pb.VMData{
+			ID:      int64(data.ID),
+			Name:    data.Name,
+			GroupID: int64(data.GroupID),
+			CPU:     int32(data.CPU),
+			Mem:     int32(data.Mem),
+			Storage: storage,
+			Net:     net,
+			PCIData: pci,
+		}, nil
 	}
 }
 
-func (s *server) StopVM(ctx context.Context, in *pb.VMID) (*pb.Result, error) {
-	fmt.Println("----------StopVM-----")
-	log.Printf("Receive VMID: %v", in.GetId())
-	info, result := vm.VMStop(int(in.GetId()))
-	if result == false {
-		fmt.Println("VMStop Error!!")
-		return &pb.Result{Status: false, Info: info}, nil
-	}
-
-	return &pb.Result{Status: true, Info: "ok!!"}, nil
-}
-
-func (s *server) GetVM(ctx context.Context, in *pb.VMID) (*pb.VMData, error) {
-	fmt.Println("----------GetVMID-----")
-	log.Printf("Receive VMID: %v", in.GetId())
-	result, err := db.VMDBGetData(int(in.GetId()))
-	if err != nil {
-		fmt.Println("Error!!")
-		return &pb.VMData{}, fmt.Errorf("Not Found!!")
-
-	}
-	return &pb.VMData{
-		Option: &pb.Option{
-			StoragePath: result.StoragePath,
-			Vnc:         int32(result.Vnc),
-			Id:          int64(result.ID),
-			Autostart:   result.AutoStart,
-			Status:      int32(result.Status),
-		},
-		Vmname: result.Name,
-		Vcpu:   int64(result.CPU),
-		Vmem:   int64(result.Mem),
-		Vnet:   result.Net,
-	}, nil
-}
-
-func (s *server) GetVMName(ctx context.Context, in *pb.VMName) (*pb.VMData, error) {
-	fmt.Println("----------GetVMName-----")
-	log.Printf("Receive Name: %v", in.GetVmname())
-	id, err := db.VMDBGetVMID(in.GetVmname())
-	if err != nil {
-		fmt.Println("NotFound VMID !!")
-		return &pb.VMData{}, fmt.Errorf("Not Found VMID!!")
-	}
-	result, err := db.VMDBGetData(id)
-	if err != nil {
-		fmt.Println("Not Found!!")
-		return &pb.VMData{}, fmt.Errorf("Not Found!!")
-
-	}
-	return &pb.VMData{
-		Option: &pb.Option{
-			StoragePath: result.StoragePath,
-			Vnc:         int32(result.Vnc),
-			Id:          int64(result.ID),
-			Autostart:   result.AutoStart,
-		},
-		Vmname:  result.Name,
-		Vcpu:    int64(result.CPU),
-		Vmem:    int64(result.Mem),
-		Vnet:    result.Net,
-		Storage: result.Storage,
-	}, nil
-}
-
-//func (s *server) GetAllVM(ctx context.Context, in *pb.VMID)  error {
-//	log.Println("----GetAllVM----")
-//	log.Printf("Receive GetAllVM")
-//	fmt.Println(db.VMDBGetAll())
-//	return nil
-//}
-
-func (s *server) GetAllVM(base *pb.Base, stream pb.Grpc_GetAllVMServer) error {
+func (s *server) GetAllVM(_ *pb.Null, stream pb.Node_GetAllVMServer) error {
 	log.Println("----GetAllVM----")
 	log.Printf("Receive GetAllVM")
-	fmt.Println(db.VMDBGetAll())
-	result := db.VMDBGetAll()
-	for _, a := range result {
-		if err := stream.Send(&pb.VMData{Option: &pb.Option{
-			Vnc: int32(a.Vnc), Id: int64(a.ID), Autostart: a.AutoStart, Status: int32(a.Status),
-			StoragePath: a.StoragePath},
-			Vmname: a.Name, Vcpu: int64(a.CPU), Vmem: int64(a.Mem), Vnet: a.Net, Storage: a.Storage}); err != nil {
-			return err
+	var storage []*pb.StorageData
+	var net []*pb.NetData
+	var pci []*pb.PCIData
+
+	if result, err := db.GetAllDBVM(); err != nil {
+		return err
+	} else {
+		log.Println(result)
+		for _, data := range result {
+			for _, tmp := range strings.Split(data.Storage, ",") {
+				value, _ := strconv.Atoi(tmp)
+				storage = append(storage, &pb.StorageData{ID: int64(value)})
+			}
+			for _, tmp := range strings.Split(data.Net, ",") {
+				value, _ := strconv.Atoi(tmp)
+				net = append(net, &pb.NetData{ID: int64(value)})
+			}
+			//for _, tmp := range strings.Split(data.PCI, ",") {
+			//	value, _ := strconv.Atoi(tmp)
+			//	pci = append(pci, &pb.PCIData{ID: int64(value)})
+			//}
+			if err := stream.Send(&pb.VMData{
+				ID:      int64(data.ID),
+				Name:    data.Name,
+				GroupID: int64(data.GroupID),
+				CPU:     int32(data.CPU),
+				Mem:     int32(data.Mem),
+				Storage: storage,
+				Net:     net,
+				PCIData: pci,
+			}); err != nil {
+				return err
+			}
 		}
-	}
-	return nil
-}
-
-func (s *server) ShutdownVM(ctx context.Context, in *pb.VMID) (*pb.Result, error) {
-	log.Println("----ShutdownVM----")
-	log.Printf("Receive ShutdownVM")
-	data, err := db.VMDBGetData(int(in.GetId()))
-	if err != nil {
-		fmt.Println("ID Not Found !!")
-		return &pb.Result{Status: false, Info: "ID Not Found!!"}, nil
-	}
-	if vm.VMShutdown(data.Name) {
-		return &pb.Result{Status: true, Info: "ok"}, nil
-	} else {
-		return &pb.Result{Status: false, Info: "Error: Shutdown Error!!"}, nil
-	}
-}
-
-func (s *server) ResetVM(ctx context.Context, in *pb.VMID) (*pb.Result, error) {
-	log.Println("----RebootVM----")
-	log.Printf("Receive RebootVM")
-	data, err := db.VMDBGetData(int(in.GetId()))
-	if err != nil {
-		fmt.Println("ID Not Found !!")
-		return &pb.Result{Status: false, Info: "ID Not Found!!"}, nil
-	}
-	if vm.VMRestart(data.Name) {
-		return &pb.Result{Status: true, Info: "ok"}, nil
-	} else {
-		return &pb.Result{Status: false, Info: "Restart Error!!"}, nil
-	}
-}
-
-func (s *server) PauseVM(ctx context.Context, in *pb.VMID) (*pb.Result, error) {
-	log.Println("----PauseVM----")
-	log.Printf("Receive PauseVM")
-	data, err := db.VMDBGetData(int(in.GetId()))
-	if err != nil {
-		fmt.Println("ID Not Found !!")
-		return &pb.Result{Status: false, Info: "ID Not Found!!"}, nil
-	}
-	if vm.VMPause(data.Name) {
-		db.VMDBStatusUpdate(data.ID, 2)
-		return &pb.Result{Status: true, Info: "ok"}, nil
-	} else {
-		return &pb.Result{Status: false, Info: "Pause VM Error!!"}, nil
-	}
-}
-
-func (s *server) ResumeVM(ctx context.Context, in *pb.VMID) (*pb.Result, error) {
-	log.Println("----ResumeVM----")
-	log.Printf("Receive ResumeVM")
-	data, err := db.VMDBGetData(int(in.GetId()))
-	if err != nil {
-		fmt.Println("ID Not Found !!")
-		return &pb.Result{Status: false, Info: "ID Not Found!!"}, nil
-	}
-	if vm.VMResume(data.Name) {
-		return &pb.Result{Status: true, Info: "ok"}, nil
-	} else {
-		return &pb.Result{Status: false, Info: "Resume VM Error!!"}, nil
+		return nil
 	}
 }
