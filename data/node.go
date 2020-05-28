ce9a27fb2ca3a173921e4daadab8e6339a6eaf3f
yoneyan <github@yoneyan.net>
2020-05-28 17:03:12 +0900

    fixed node stop process

--- data/node.go
+++ data/node.go
@@ -2,17 +2,17 @@ package data
 
 import (
 	"context"
-	"github.com/yoneyan/vm_mgr/node/vm"
-	pb "github.com/yoneyan/vm_mgr/proto/proto-go"
+	//"github.com/vmmgr/node/vm"
+	pb "github.com/vmmgr/node/proto/proto-go"
 	"log"
 	"time"
 )
 
-func (s *server) StopNode(ctx context.Context, in *pb.NodeID) (*pb.Result, error) {
+func (s *server) StopNode(_ context.Context, _ *pb.Null) (*pb.Result, error) {
 	log.Println("----StopNode----")
-	vm.StopProcess()
+	//vm.StopProcess()
 	timer := time.NewTimer(time.Second * 1)
 	<-timer.C
-	log.Printf("Node End! ")
+	log.Printf("Stop Node End! ")
 	return &pb.Result{Status: true}, nil
 }
