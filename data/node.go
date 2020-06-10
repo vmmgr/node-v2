d5248a5888bbabb92469f9672c2788d690bfa67e
yoneyan <github@yoneyan.net>
2020-06-10 16:43:57 +0900

    add stop process

--- data/node.go
+++ data/node.go
@@ -2,15 +2,16 @@ package data
 
 import (
 	"context"
-	//"github.com/vmmgr/node/vm"
 	pb "github.com/vmmgr/node/proto/proto-go"
+	"github.com/yoneyan/vm_mgr/node/vm"
 	"log"
 	"time"
 )
 
 func (s *server) StopNode(_ context.Context, _ *pb.Null) (*pb.Result, error) {
 	log.Println("----StopNode----")
-	//vm.StopProcess()
+	//Stop Force
+	vm.StopProcess()
 	timer := time.NewTimer(time.Second * 1)
 	<-timer.C
 	log.Printf("Stop Node End! ")
