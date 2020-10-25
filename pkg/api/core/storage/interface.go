8e6e52de1e9610b674142b60604ddd3f3462510c
yoneyan <github@yoneyan.net>
2020-10-26 05:13:36 +0900

    storage interfaceの追加

---
+++ pkg/api/core/storage/interface.go
@@ -0,0 +1,29 @@
+package storage
+
+//Storage Type
+//0: CDROM
+//10: BootDevice(VirtIO)
+//11: BootDevice(SATA)
+//12: BootDevice(IDE)
+
+type Storage struct {
+	Type     uint   `json:"type"`
+	FileType uint   `json:"file_type"`
+	Path     string `json:"path"`
+	ReadOnly bool   `json:"readonly"`
+	Boot     uint   `json:"boot"`
+}
+
+type GenerateStorageXml struct {
+	Storage       Storage
+	Number        uint
+	AddressNumber uint
+}
+
+func GetDriverName(driver uint) string {
+	// デフォルトはqcow2
+	if driver == 0 {
+		return "img"
+	}
+	return "qcow2"
+}
