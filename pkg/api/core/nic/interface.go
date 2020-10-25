f8033d4e6588e8f89e278f4f44cc4762a3d8ff2f
yoneyan <github@yoneyan.net>
2020-10-26 05:13:12 +0900

    nic interfaceの追加

---
+++ pkg/api/core/nic/interface.go
@@ -0,0 +1,49 @@
+package nic
+
+const (
+	s = 0
+)
+
+//Type
+//0: Bridge 1: NAT 2:macvtap
+//Mode
+//0: Bridge 1: vpea 2: private 3: passthrough
+
+type NIC struct {
+	Type   uint   `json:"type"`
+	Driver uint   `json:"driver"`
+	Mode   uint   `json:"mode"`
+	MAC    string `json:"mac"`
+	Device string `json:"device"`
+}
+
+type GenerateNICXml struct {
+	NIC           NIC
+	Number        uint
+	AddressNumber uint
+}
+
+func GetDriverName(driver uint) string {
+	//デフォルトはvirtio
+
+	if driver == 0 {
+		return "e1000e"
+	} else if driver == 1 {
+		return "rtl8139"
+	}
+	return "virtio"
+}
+
+//Mode
+func GetModeName(mode uint) string {
+	//デフォルトはBrdige
+
+	if mode == 1 {
+		return "vepa"
+	} else if mode == 2 {
+		return "private"
+	} else if mode == 3 {
+		return "passthrough"
+	}
+	return "bridge"
+}
