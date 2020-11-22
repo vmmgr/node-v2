284d537d8f6c0db192b660c7cec6c026a40e8df4
yoneyan <github@yoneyan.net>
2020-11-22 19:20:36 +0900

    コンフィグの読み込み機能の追加

--- pkg/cmd/start.go
+++ pkg/cmd/start.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"github.com/spf13/cobra"
 	gateway "github.com/vmmgr/node/pkg/api/core/gateway/v0"
+	"github.com/vmmgr/node/pkg/api/core/tool/config"
 	"log"
 )
 
@@ -11,6 +12,14 @@ var startCmd = &cobra.Command{
 	Short: "start client server",
 	Long:  ``,
 	Run: func(cmd *cobra.Command, args []string) {
+		confPath, err := cmd.Flags().GetString("config")
+		if err != nil {
+			log.Fatalf("could not greet: %v", err)
+		}
+
+		if config.GetConfig(confPath) != nil {
+			log.Fatalf("error config process |%v", err)
+		}
 
 		gateway.NodeAPI()
 
@@ -20,4 +29,5 @@ var startCmd = &cobra.Command{
 
 func init() {
 	rootCmd.AddCommand(startCmd)
+	startCmd.PersistentFlags().StringP("config", "c", "", "config path")
 }
