8f0acb18b71c11c87bfc6d48555791debe4b36fa
yoneyan <github@yoneyan.net>
2020-11-22 19:59:51 +0900

    データ構造の大幅な変更

--- pkg/api/core/storage/interface.go
+++ pkg/api/core/storage/interface.go
@@ -1,17 +1,34 @@
 package storage
 
-//Storage Type
-//0: CDROM
-//10: BootDevice(VirtIO)
-//11: BootDevice(SATA)
-//12: BootDevice(IDE)
+var Broadcast = make(chan FileTransfer)
 
 type Storage struct {
-	Type     uint   `json:"type"`
-	FileType uint   `json:"filetype"`
-	Path     string `json:"path"`
-	ReadOnly bool   `json:"readonly"`
-	Boot     uint   `json:"boot"`
+	Mode       uint   `json:"mode"`        //0:Manual 1:From ImaCon
+	FromImaCon ImaCon `json:"from_imacon"` //Imageをpullする際に使用するURL
+	Type       uint   `json:"type"`        //0: CDROM 1:Floppy (no support) 10:BootDev(VirtIO) 11: BootDev(SATA) 12: BootDev(IDE)
+	FileType   uint   `json:"filetype"`    //0:qcow2 1:img
+	GroupID    uint   `json:"group_id"`    //コントローラへの通知先で判別するためのGroupID ID=0の場合は管理者
+	UUID       string `json:"uuid"`        //コントローラへの通知先で判別するためのUUID
+	Controller string `json:"controller"`  //コントローラへの通知先のIPアドレスとPort番号
+	PathType   uint   `json:"path_type"`   //node側のストレージの種類 0~9:SSD 10~19:HDD 20~29:NVMe 100~109:SSD(iSCSI) 110~119:SSD(iSCSI) 120~129:NVme(iSCSI)
+	Path       string `json:"path"`        //node側のパス
+	Capacity   uint   `json:"capacity"`    //容量
+	ReadOnly   bool   `json:"readonly"`    //Readonlyであるか
+	Boot       uint   `json:"boot"`
+}
+
+type ImaCon struct {
+	IP   string `json:"ip"`
+	User string `json:"user"`
+	Pass string `json:"pass"`
+	Path string `json:"path"`
+}
+
+type Convert struct {
+	SrcFile string `json:"src_file"`
+	SrcType string `json:"src_type"`
+	DstFile string `json:"dst_file"`
+	DstType string `json:"dst_type"`
 }
 
 type GenerateStorageXml struct {
@@ -20,9 +37,28 @@ type GenerateStorageXml struct {
 	AddressNumber uint
 }
 
-func GetDriverName(driver uint) string {
+type FileTransfer struct {
+	URL         string
+	CurrentSize int64
+	AllSize     int64
+}
+
+type SFTPAuth struct {
+	IP   string
+	User string
+	Pass string
+}
+
+type SendController struct {
+	GroupID  uint   `json:"group_id"`
+	UUID     string `json:"uuid"`
+	Progress uint   `json:"progress"`
+	Comment  uint   `json:"comment"`
+}
+
+func GetExtensionName(extension uint) string {
 	// デフォルトはqcow2
-	if driver == 1 {
+	if extension == 1 {
 		return "img"
 	}
 	return "qcow2"
